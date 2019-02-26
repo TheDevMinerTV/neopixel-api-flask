dcb275afbd6717392e551f2bc36f94628abe34d0
TheDevMinerTV <tobigames200@gmail.com>
2019-02-26 10:01:53 +0100

    Remove unneeded comments from controller

--- controller.py
+++ controller.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
-# NeoPixel library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
+#!/usr/bin/python2.7
 
 import time
 from neopixel import *
@@ -13,7 +8,7 @@ import requests
 # LED strip configuration:
 LED_COUNT      = 150     # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+# LED_PIN      = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
@@ -23,7 +18,6 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 url = 'http://192.168.5.130:5000/api/neopixels/all/get'
 
 def colorUpdate(strip, r):
-    """Wipe color across display a pixel at a time."""
     data = r.json()
     for i in data:
 	strip.setPixelColor(i['id'], Color(i['values']['r'], i['values']['g'], i['values']['b']))
